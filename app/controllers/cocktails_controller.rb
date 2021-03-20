68da575a5595e0cb1fd7de0e4c0099aa16770704
Sami Ziani <zianisami0@gmail.com>
2021-03-20 23:03:54 +0100

    correct delete cocktail error

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -31,7 +31,7 @@ class CocktailsController < ApplicationController
   def destroy
     @cocktail = Cocktail.find(params[:id])
     @cocktail.destroy
-    redirect_to cocktails_path
+    redirect_to root_path
   end
 
   private
